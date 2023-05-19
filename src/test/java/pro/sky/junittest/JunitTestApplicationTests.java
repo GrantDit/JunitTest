41a8b94fb43639bcffc557e412dab62a70aeb1b5
GrantDit <kiruha5854@mail.ru>
2023-05-20 02:48:16 +0300

    FirstCommitTestJUnit

---
+++ src/test/java/pro/sky/junittest/JunitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.junittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
